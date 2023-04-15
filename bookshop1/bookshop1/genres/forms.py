2e7f4dce9252dcb90582e7d996bb307b1497b206
ducmanh02 <89408872+ducmanh02@users.noreply.github.com>
2023-04-15 15:15:28 +0700

    loadding 99%

--- bookshop1/bookshop1/genres/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Genres
-
-class GenreForm(forms.ModelForm):
-    class Meta:
-        model = Genres
-        fields = ('name',)
--- bookshop1/bookshop1/users/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class UserProfile(models.Model):
-    user = models.OneToOneField('auth.User', on_delete=models.CASCADE, related_name='profile')
-    address = models.TextField()
-    phone_number = models.CharField(max_length=20)
-
-    def __str__(self):
-        return f"{self.user.username}'s profile"
